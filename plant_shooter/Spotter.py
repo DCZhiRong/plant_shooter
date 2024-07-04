5b50ec5d23de99cc99d856e6ffbb34248dc0bc08
Dan <interwin.daniel@gmail.com>
2024-07-05 06:40:13 +0800

    servo

--- plant_shooter/Spotter.py
+++ plant_shooter/Spotter.py
@@ -76,8 +76,8 @@ class ImagePublisher(Node):
     # image to a ROS 2 image message
     self.publisher_.publish(self.br.cv2_to_imgmsg(frame, 'bgr8'))
     if objectInfo:
-      x_error = 640-(objectInfo[0][0][0]+objectInfo[0][0][2])
-      y_error = 480-(objectInfo[0][0][1]+objectInfo[0][0][3])
+      x_error = 640-(objectInfo[0][0][0]+objectInfo[0][0][2]/2)
+      y_error = 360-(objectInfo[0][0][1]+objectInfo[0][0][3]/2)
       curT = time.time()
       time_diff = curT-self.prevT
       self.preT = curT
