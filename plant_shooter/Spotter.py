473b51a3312be393128a17f944e8b7bc5c5b4324
Dan <interwin.daniel@gmail.com>
2024-07-05 06:33:54 +0800

    servo

--- plant_shooter/Spotter.py
+++ plant_shooter/Spotter.py
@@ -88,7 +88,7 @@ class ImagePublisher(Node):
       self.prex = x_error
       self.prey = y_error
       ux = p*x_error + i*self.ex + d*dedtX
-      uy = p*y_error + i*self.ey + d*dedtY
+      uy = 2*p*y_error + i*self.ey + d*dedtY
       ux = min(60, max(-60, ux))
       uy = min(60, max(-60, -uy))
       print(y_error)
