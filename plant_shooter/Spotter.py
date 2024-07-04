50bbf3b03d85a2788e6f6a27545fad82448bfa72
Daniel <interwin.daniel@gmail.com>
2024-07-04 16:50:52 +0800

    opencv with ros

--- plant_shooter/Spotter.py
+++ plant_shooter/Spotter.py
@@ -1,74 +1,84 @@
 #! /usr/bin/env python3
-
+import os
 import rclpy # Python Client Library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
+from ament_index_python.packages import get_package_share_directory
+bringup_dir = get_package_share_directory('plant_shooter')
+classNames = []
+classFile = os.path.join(bringup_dir, 'Object_Detection_Files', 'coco.names')
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+configPath = os.path.join(bringup_dir, 'Object_Detection_Files', 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+weightsPath = os.path.join(bringup_dir, 'Object_Detection_Files', 'frozen_inference_graph.pb')
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
 class ImagePublisher(Node):
-  """
-  Create an ImagePublisher class, which is a subclass of the Node class.
-  """
+
   def __init__(self):
-    """
-    Class constructor to set up the node
-    """
-    # Initiate the Node class's constructor and give it a name
+
     super().__init__('image_publisher')
-      
-    # Create the publisher. This publisher will publish an Image
-    # to the video_frames topic. The queue size is 10 messages.
+
     self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
-      
-    # We will publish a message every 0.1 seconds
-    timer_period = 0.1  # seconds
-      
-    # Create the timer
+
+    timer_period = 0.02  # seconds
+
     self.timer = self.create_timer(timer_period, self.timer_callback)
          
-    # Create a VideoCapture object
-    # The argument '0' gets the default webcam.
     self.cap = cv2.VideoCapture(0)
-         
+    self.cap.set(3,640)
+    self.cap.set(4,480)
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
    
   def timer_callback(self):
-    """
-    Callback function.
-    This function gets called every 0.1 seconds.
-    """
-    # Capture frame-by-frame
-    # This method returns True/False as well
-    # as the video frame.
-    ret, frame = self.cap.read()
-          
-    if ret == True:
-      # Publish the image.
-      # The 'cv2_to_imgmsg' method converts an OpenCV
-      # image to a ROS 2 image message
-      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
- 
+    ret, frame = self.cap.read()       
+    result, objectInfo = self.getObjects(frame,0.5,0.2, objects=['cell phone'])
+    # Publish the image.
+    # The 'cv2_to_imgmsg' method converts an OpenCV
+    # image to a ROS 2 image message
+    self.publisher_.publish(self.br.cv2_to_imgmsg(frame, 'bgr8'))
+    if objectInfo:
+      print(objectInfo[0][0][0]+objectInfo[0][0][2]/2)
+      print(objectInfo[0][0][1]+objectInfo[0][0][3]/2)
     # Display the message on the console
-    self.get_logger().info('Publishing video frame')
-  
+    # self.get_logger().info('Publishing video frame')
+
+  def getObjects(self, img, thres, nms, draw=True, objects=[]):
+      classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+  #Below has been commented out, if you want to print each sighting of an object to the console you can uncomment below     
+      if len(objects) == 0: objects = classNames
+      objectInfo =[]
+      if len(classIds) != 0:
+          for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+              className = classNames[classId - 1]
+              if className in objects: 
+                  objectInfo.append([box,className])
+                  if (draw):
+                      cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                      cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                      cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                      cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                      cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+      
+      return img,objectInfo
+
 def main(args=None):
-  
-  # Initialize the rclpy library
   rclpy.init(args=args)
-  
-  # Create the node
   image_publisher = ImagePublisher()
-  
   # Spin the node so the callback function is called.
   rclpy.spin(image_publisher)
-  
   # Destroy the node explicitly
   # (optional - otherwise it will be done automatically
   # when the garbage collector destroys the node object)
   image_publisher.destroy_node()
-  
   # Shutdown the ROS client library for Python
   rclpy.shutdown()
   
--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
 
 package_name = 'plant_shooter'
 
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'Object_Detection_Files'), glob('Object_Detection_Files/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'Spotter = plant_shooter.Spotter:main'
         ],
     },
 )
