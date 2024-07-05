08b1a90405b620ed74eab8c150a8b1b74142f27d
Dan <interwin.daniel@gmail.com>
2024-07-05 09:53:04 +0800

    servo tuning

--- plant_shooter/Spotter.py
+++ plant_shooter/Spotter.py
@@ -45,7 +45,7 @@ class ImagePublisher(Node):
 
     self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
 
-    timer_period = 0.01  # seconds
+    timer_period = 0.03  # seconds
 
     self.timer = self.create_timer(timer_period, self.timer_callback)
          
